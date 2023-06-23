2b729fcb3c8a5212822a9fe37b16122c4957e6bd
yifenrui <1445494883@qq.com>
2023-06-23 08:21:13 +0800

    测试日志-1

--- mp/src/main/java/com/example/demo/MpApplication.java
+++ mp/src/main/java/com/example/demo/MpApplication.java
@@ -1,15 +1,17 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MpApplication.class, args);
 
-
+		log.info("");
 	}
 
 }
